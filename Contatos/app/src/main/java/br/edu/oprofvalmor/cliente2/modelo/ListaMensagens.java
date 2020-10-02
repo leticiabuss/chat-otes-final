d026659edc121637a3984ec6f40d279fabc87185
Letícia Buss <leticia.souzabuss@gmail.com>
2020-10-01 22:06:56 -0300

    splashScreen funcionando

--- Contatos/app/src/main/java/br/edu/oprofvalmor/cliente2/modelo/ListaMensagens.java
+++ Contatos/app/src/main/java/br/edu/oprofvalmor/cliente2/modelo/ListaMensagens.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class ListaMensagens {
     private static ObservableField<List<String>> listaMensagens = new ObservableField<List<String>>();
-
     public static ObservableField<List<String>> getListaDeMensagens() {
         return listaMensagens;
     }
